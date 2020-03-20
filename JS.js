50f2e29afb158092ebbf31e3230c44d2c4f1228a
Em Calderon <calderonhome@Ems-iMac.local>
2020-03-19 20:16:34 -0700

    jQuery including all seasons (doesn't work)

--- JS.js
+++ JS.js
@@ -2,13 +2,30 @@ $(document).ready(() => {
   let $pictures = $(".svg");
   $pictures.hide();
 
-  $("#button").click(() => {showSeason(val)});
+  let $button = $("#button");
+    $button.on({
+        click: () => {
+            showSeason(val);
+        }
+    });
 });
 
 function showSeason(val){
     let $name = $("#type");
-    if ($name.text(val)== "spring" || $name.text(val)== "Spring") {
-        let $topSpring = $("#spring1");
-        return $topSpring.show();
+    if ($name.text(val)= "spring" ) {
+        let $spring = $(".spring");
+        $spring.show();
+    }
+    if ($name.text(val)= "summer" ) {
+        let $summer = $(".summer");
+        $summer.show();
+    }
+    if ($name.text(val)= "autumn" ) {
+        let $autumn = $(".autumn");
+        $autumn.show();
+    }
+    if ($name.text(val)= "winter" ) {
+        let $winter = $(".winter");
+        $winter.show();
     }
 }
