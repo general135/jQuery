ce52440f176b75a3dbbb977c8a829184c309d208
Em Calderon <calderonhome@Ems-iMac.local>
2020-03-19 19:54:54 -0700

    more jQuery code

--- JS.js
+++ JS.js
@@ -1,4 +1,14 @@
 $(document).ready(() => {
   let $pictures = $(".svg");
   $pictures.hide();
+
+  $("#button").click(() => {showSeason(val)});
 });
+
+function showSeason(val){
+    let $name = $("#type");
+    if ($name.text(val)== "spring" || $name.text(val)== "Spring") {
+        let $topSpring = $("#spring1");
+        return $topSpring.show();
+    }
+}
