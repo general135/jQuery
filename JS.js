bd53385d0c60cbaea5b71b5ad0e8fba737f95694
Em Calderon <calderonhome@Ems-iMac.local>
2020-03-20 22:52:23 -0700

    added background color and centered the graphics

--- JS.js
+++ JS.js
@@ -26,26 +26,5 @@ $button.on({
     }
     }
   });
-
 });
-
-function showSeason() {
-    let $name = $("#text").val();
-    if ( $name == "spring" || $name == "Spring" ) {
-        let $spring = $(".spring");
-        $spring.show();
-    }
-    if ($name == "summer" || $name == "Summer") {
-        let $summer = $(".summer");
-        $summer.show();
-    }
-    if ($name == "autumn" || $name == "Autumn" || $name == "fall" || $name == "Fall") {
-        let $autumn = $(".autumn");
-        $autumn.show();
-    }
-    if ( $name == "winter" || $name == "Winter") {
-        let $winter = $(".winter");
-        $winter.show();
-    }
-}
 
